4c0e77928fcb8b4ad57436e0e877ab8b3caed6d4
Briana <brea83@gmail.com>
2025-01-13 12:00:24 +0100

    game design notes for class assignment

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -6,8 +6,23 @@
  *      teams try to get the most areas controlled in a time limit
  *      is control taken instantly, or does it take time?
  * 
- * player feel
+ *
  *      should players be able to push eachother?
- *      do we have tag or attacks?
- * 
+ *      
+ *      Tag?
+ *          1 person is IT for each team?
+ *                  IT cannot take territory, other teammates can
+ *                  tagging ppl builds their team?
+ *                  other players are
+ *                  
+ *          1 person is IT at a time
+ *                  tagging a person takes their territory, and makes them it?
+ *              
+ *          Tagging a player makes you swap? everyone is it?
+ *              
+ *      sardines variation
+ *          one player hides. other players must be touching the hider or another player that is touching the hider to get a point at the end of the round
+ *              last person to find them is next it, or random selection from players who failed to find them
+ *              
+ *          players should make sounds when they bump into things, maybe they join a "team" of hiders while in contact with another hider
  */
