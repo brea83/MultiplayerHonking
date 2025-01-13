e026db3357552c2a63160d382706882aee5e51f4
Briana <brea83@gmail.com>
2025-01-13 10:51:08 +0100

    Moved files around in folders and added notes file
    I am still not convinced this folder structure is the right one

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,13 @@
+/*
+ * 
+ * do notes here
+ * 
+ * area control
+ *      teams try to get the most areas controlled in a time limit
+ *      is control taken instantly, or does it take time?
+ * 
+ * player feel
+ *      should players be able to push eachother?
+ *      do we have tag or attacks?
+ * 
+ */
